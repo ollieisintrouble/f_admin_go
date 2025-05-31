dec43ae7700cc0470cd7b270eef10f5dc425b244
Oliver Nguyen <olivernguyen@192.168.1.30>
2025-06-01 04:07:43 +0700

    fix connection

--- internal/api/shared/base_handler.go
+++ internal/api/shared/base_handler.go
@@ -14,7 +14,7 @@ type EntityHandler interface {
 
 func HandleEntity(h EntityHandler, authenticator *SimpleAuthenticator) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		w.Header().Set("Access-Control-Allow-Origin", "https://f-admin-sv.vercel.app/")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
